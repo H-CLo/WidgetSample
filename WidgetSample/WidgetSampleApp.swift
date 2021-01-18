dabce55f624405d3b414bc246d1881887bb5b02a
Willey <willey.lo@yuanta.com>
2021-01-18 08:22:33 +0800

    First commit

---
+++ WidgetSample/WidgetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSampleApp.swift
+//  WidgetSample
+//
+//  Created by Willey on 2021/1/18.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
